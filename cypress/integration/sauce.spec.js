deb8543ce1ab0093ef7c45018c5b156f29e75027
afif hendrawan <afif.hendrawan@gmail.com>
2022-06-07 00:31:40 +0700

    update

--- cypress/integration/sauce.spec.js
+++ cypress/integration/sauce.spec.js
@@ -10,7 +10,7 @@ describe('Login Page - Standard User Suite', () => {
         cy.get('#user-name').type('standard_user')
         cy.get('#password').type('secret_sauce')
         cy.get('#login-button').click()
-        cy.contains('Products').should('be.visible')
+        cy.contains('Producted').should('be.visible')
     })
 
     it('Check for Standard User - Invalid username', () => {
